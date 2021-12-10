d3445cb48b6d9cf07e4f6f7348e79b951cd4c5ed
celiaxiao <c6xiao@ucsd.edu>
2021-12-09 19:41:05 -0800

    fix random pillar and bonus; update bird2 color

--- src/Model.hs
+++ src/Model.hs
@@ -1,76 +1,87 @@
 {-# LANGUAGE RecordWildCards #-}
-module Model where 
 
-import Prelude hiding ((!!))
-import qualified Model.Board  as Board
-import qualified Model.Score  as Score
-import qualified Model.Player as Player
+module Model where
 
+import Brick hiding (Direction)
+import qualified Brick.AttrMap as A
+import qualified Brick.Focus as F
 import qualified Brick.Widgets.Dialog as D
 import qualified Brick.Widgets.Edit as E
-import qualified Brick.Focus as F
-
-import Linear.V2 (V2 (..), _x, _y)
-import System.IO
-import Brick hiding (Direction)
-import qualified Graphics.Vty as V
-import qualified Brick.AttrMap as A
-
 import Data.Sequence hiding (fromList)
 import qualified Data.Sequence as S
-import System.Random (Random (..), getStdRandom, newStdGen)
+import qualified Graphics.Vty as V
+import Linear.V2 (V2 (..))
+import System.IO
+import System.Random (Random (..), getStdRandom, randomRIO)
+import Prelude hiding ((!!))
+
 -- import View (height, width, gapSize, offset)
 
 -------------------------------------------------------------------------------
+
 -- | Ticks mark passing of time: a custom event that we constantly stream
+
 -------------------------------------------------------------------------------
 data Tick = Tick
 
 -------------------------------------------------------------------------------
+
 -- | Top-level App State ------------------------------------------------------
+
 -------------------------------------------------------------------------------
 
-data State 
-  = Intro 
-  | Play PlayState 
-  | Outro 
+data State
+  = Intro
+  | Play PlayState
+  | Outro
 
-data St =
-    St { _focusRing :: F.FocusRing String
-       , _edit1 :: E.Editor String String
-       , _edit2 :: E.Editor String String
-       }
+data St = St
+  { _focusRing :: F.FocusRing String,
+    _edit1 :: E.Editor String String,
+    _edit2 :: E.Editor String String
+  }
 
 type Coord = V2 Int
 
 data Stream a = a :| Stream a
   deriving (Show)
 
-height, width, gapSize, offset :: Int
+height, width, gapSize, offset, birdXPos, pListLen, bListLen :: Int
 height = 30
 width = 30
 gapSize = height * 3 `div` 10
 offset = height `div` 6
 birdXPos = width `div` 4
+pListLen = 100
+bListLen = 30
 
 -- gameOverAttr, snakeAttr, birdAttr, emptyAttr, bonusAttr :: AttrName
 gameOverAttr = attrName "gameOver"
+
 snakeAttr = attrName "snakeAttr"
+
 birdAttr = attrName "birdAttr"
+
 emptyAttr = attrName "emptyAttr"
+
 bonusAttr = attrName "bonusAttr"
 
+bird2Attr = attrName "bird2Attr"
+
 theMap :: A.AttrMap
-theMap = A.attrMap V.defAttr
-    [ (D.dialogAttr, V.white `on` V.blue)
-    , (D.buttonAttr, V.black `on` V.white)
-    , (D.buttonSelectedAttr, bg V.yellow)
-    , (E.editAttr, V.white `on` V.blue)
-    , (E.editFocusedAttr, V.black `on` V.yellow)
-    , (snakeAttr, V.blue `on` V.blue)
-    , (birdAttr, V.red `on` V.red)
-    , (bonusAttr, V.yellow `on` V.yellow)
-    , (gameOverAttr, fg V.red `V.withStyle` V.bold)
+theMap =
+  A.attrMap
+    V.defAttr
+    [ (D.dialogAttr, V.white `on` V.blue),
+      (D.buttonAttr, V.black `on` V.white),
+      (D.buttonSelectedAttr, bg V.yellow),
+      (E.editAttr, V.white `on` V.blue),
+      (E.editFocusedAttr, V.black `on` V.yellow),
+      (snakeAttr, V.blue `on` V.blue),
+      (birdAttr, V.red `on` V.red),
+      (bonusAttr, V.yellow `on` V.yellow),
+      (bird2Attr, V.green `on` V.green),
+      (gameOverAttr, fg V.red `V.withStyle` V.bold)
     ]
 
 data Direction
@@ -81,37 +92,34 @@ data Direction
   deriving (Eq, Show)
 
 data PlayState = PS
-  { gameState :: Int
-  , isNetwork :: Int
-  , isServer :: Int
-  , choices :: D.Dialog Int
-  , choices2 :: D.Dialog Int
-  , st :: St
-  , st2msg :: String
-  , ip :: String
-  , port :: String
-  , birdY :: Int
-  , historyscore :: [Int]
-  , bonus :: Coord
-  , pl1 :: Int
-  , pl2 :: Int
-  , pl3 :: Int
-  , x1 :: Int
-  , x2 :: Int
-  , x3 :: Int
-  , wall :: Int
-  , randP :: Int
-  , randPs :: Stream Int
-  , bird1 :: Seq Coord
-  , bird2 :: Seq Coord
-  , score :: Int
-  , bonusList :: Stream Coord
-  , dir :: Direction
-  , nextBonus :: Coord
+  { gameState :: Int,
+    isNetwork :: Int,
+    isServer :: Int,
+    choices :: D.Dialog Int,
+    choices2 :: D.Dialog Int,
+    st :: St,
+    st2msg :: String,
+    birdY :: Int,
+    historyscore :: [Int],
+    bonus :: Coord,
+    pl1 :: Int,
+    pl2 :: Int,
+    pl3 :: Int,
+    x1 :: Int,
+    x2 :: Int,
+    x3 :: Int,
+    randP :: Int,
+    randPs :: [Int],
+    bird1 :: Seq Coord,
+    bird2 :: Seq Coord,
+    score :: Int,
+    bonusList :: [Int],
+    dir :: Direction,
+    nextBonus :: Int
   }
 
 -- init :: Int -> PlayState
--- init n = PS 
+-- init n = PS
 --   { gameState = 0
 --   , isNetwork = 0
 --   , isServer = 0
@@ -130,23 +138,31 @@ drawInt x y = getStdRandom (randomR (x, y))
 fromList :: [a] -> Stream a
 fromList = foldr (:|) (error "Streams must be infinite")
 
+randomList :: Int -> Int -> Int -> IO [Int]
+randomList 0 _ _ = return []
+randomList n u l = do
+  r <- randomRIO (u, l)
+  rs <- randomList (n -1) u l
+  return (r : rs)
 
 -- initGame :: IO Game
 initGame = do
   -- contents <- readFile "/home/cse230/Desktop/test.txt"
-  (bo :| bs) <-
-    fromList . randomRs (V2 birdXPos (height `div` 4), V2 birdXPos (height * 3 `div` 4)) <$> newStdGen
-  -- streaming of random pillar length
-  (randp :| randps) <-
-    fromList . randomRs (0 + offset, (height `div` 3) + offset) <$> newStdGen
+  -- (bo :| bs) <-
+  --   fromList . randomRs (V2 birdXPos (height `div` 4), V2 birdXPos (height * 3 `div` 4)) <$> newStdGen
+  -- -- streaming of random pillar length
+  -- (randp :| randps) <-
+  --   fromList . randomRs (0 + offset, (height `div` 3) + offset) <$> newStdGen
+
   -- hard code initial pillar length
   a <- drawInt (0 + offset) ((height `div` 3) + offset)
   b <- drawInt (0 + offset) ((height `div` 3) + offset)
   c <- drawInt (0 + offset) ((height `div` 3) + offset)
+  bonusy <- drawInt (0 + offset) ((height `div` 3) + offset)
+  pillarLenList <- randomList pListLen offset ((height `div` 3) + offset)
+  bonusPosList <- randomList bListLen (height `div` 4) (height * 3 `div` 4)
   let xm = birdXPos
       ym = height `div` 2
-      bonusx = 15
-      bonusy = 15
       -- x = init $ split contents
       -- y = sort [ read a::Integer | a <-x]
       -- result = take 5 y
@@ -155,69 +171,71 @@ initGame = do
           { bird1 = S.singleton (V2 xm ym),
             bird2 = S.singleton (V2 xm ym),
             bonus = V2 birdXPos bonusy,
-            bonusList = bs,
+            bonusList = bonusPosList,
             score = 0,
             dir = South,
             historyscore = [], -- result TODO
             -- from C branch
-            randP = randp,
-            randPs = randps,
+            randP = 0,
+            randPs = pillarLenList,
             pl1 = a,
             pl2 = b,
             pl3 = c,
             x1 = width - 1,
             x2 = width * 2 `div` 3,
             x3 = width `div` 3,
-            wall = 0,
             gameState = 0,
-            isNetwork = 0, 
+            isNetwork = 0,
             isServer = 0,
             choices = initChoices,
             choices2 = initChoices2,
-            st = St (F.focusRing ["Edit1", "Edit2"])
+            st =
+              St
+                (F.focusRing ["Edit1", "Edit2"])
                 (E.editor "Edit1" Nothing "")
                 (E.editor "Edit2" (Just 2) ""),
             st2msg = "Press Enter to confirm, Esc to quit.",
             birdY = 0,
-            nextBonus = bo
+            nextBonus = 0
           }
   return g
 
 initChoices :: D.Dialog Int
 initChoices = D.dialog (Just "Choose Game Mode") (Just (0, choices)) 50
-    where
-        choices = [ ("Single", 0)
-                  , ("Double", 1)
-                  ]
+  where
+    choices =
+      [ ("Single", 0),
+        ("Double", 1)
+      ]
 
 initChoices2 :: D.Dialog Int
 initChoices2 = D.dialog (Just "Double Model Chosen") (Just (0, choices)) 50
-    where
-        choices = [ ("Start Server", 0)
-                  , ("Join Server", 1)
-                  ]
+  where
+    choices =
+      [ ("Start Server", 0),
+        ("Join Server", 1)
+      ]
 
 -- isCurr :: PlayState -> Int -> Int -> Bool
 -- isCurr s r c = Board.pRow p == r && Board.pCol p == c
---   where 
+--   where
 --     p = psPos s
 
 -- next :: PlayState -> Board.Result Board.Board -> Either (Board.Result ()) PlayState
 -- next s Board.Retry     = Right s
 -- next s (Board.Cont b') = Right (s { psBoard = b'
 --                                   , psTurn  = Board.flipXO (psTurn s) })
--- next s res             = nextBoard s res 
+-- next s res             = nextBoard s res
 
 -- nextBoard :: PlayState -> Board.Result a -> Either (Board.Result ()) PlayState
 -- nextBoard s res = case res' of
---                     Board.Win _ -> Left res' 
+--                     Board.Win _ -> Left res'
 --                     Board.Draw  -> Left res'
---                     _           -> Right s' 
---   where 
---     sc'  = Score.add (psScore s) (Board.boardWinner res) 
+--                     _           -> Right s'
+--   where
+--     sc'  = Score.add (psScore s) (Board.boardWinner res)
 --     res' = Score.winner sc'
 --     s'   = s { psScore = sc'                   -- update the score
 --              , psBoard = mempty                -- clear the board
 --              , psTurn  = Score.startPlayer sc' -- toggle start player
---              } 
-
+--              }
--- src/View.hs
+++ src/View.hs
@@ -1,36 +1,33 @@
 module View (view) where
 
 import Brick
-import Brick.Widgets.Center (center)
+import qualified Brick.AttrMap as A
+import qualified Brick.Focus as F
+import qualified Brick.Types as T
+import Brick.Util (bg, on)
 import Brick.Widgets.Border (borderWithLabel, hBorder, vBorder)
+import qualified Brick.Widgets.Border as B
 import Brick.Widgets.Border.Style (unicode)
-import Text.Printf (printf)
-
+import qualified Brick.Widgets.Border.Style as BS
+import Brick.Widgets.Center (center)
+import qualified Brick.Widgets.Center as C
+import qualified Brick.Widgets.Dialog as D
+import qualified Brick.Widgets.Edit as E
+import Graphics.Vty hiding (dim)
+import qualified Graphics.Vty as V
+import Linear.V2 (V2 (..))
 import Model
 import Model.Board
-import Graphics.Vty hiding (dim)
-
-import qualified Brick.Widgets.Dialog as D
-import qualified Brick.Widgets.Center as C
-import qualified Brick.AttrMap as A
-import Brick.Util (on, bg)
-import qualified Brick.Types as T
-import qualified Brick.Widgets.Edit as E
-import qualified Brick.Focus as F
-
-import qualified Brick.Widgets.Border as B
-import qualified Brick.Widgets.Border.Style as BS
-import Linear.V2 (V2 (..))
-import qualified Graphics.Vty as V
+import Text.Printf (printf)
 
 -------------------------------------------------------------------------------
 -- view :: PlayState -> [Widget w]
 -------------------------------------------------------------------------------
 -- 4 dead
 
-data Cell = Snake | Bird | Bonus | Empty
+data Cell = Snake | Bird | Bonus | Empty | BirdTwo
 
-view s = case (gameState s) of
+view s = case gameState s of
   0 -> view0 s
   1 -> view1 s
   2 -> view2 s
@@ -39,30 +36,31 @@ view s = case (gameState s) of
 
 -- view0 :: PlayState -> [Widget ()]
 view0 s = [ui]
-    where
-        ui = D.renderDialog (choices s) $ C.hCenter $ padAll 1 $ str "   "
+  where
+    ui = D.renderDialog (choices s) $ C.hCenter $ padAll 1 $ str "   "
 
 view2 s = [ui]
-    where
-        ui = D.renderDialog (choices2 s) $ C.hCenter $ padAll 1 $ str "   "
+  where
+    ui = D.renderDialog (choices2 s) $ C.hCenter $ padAll 1 $ str "   "
 
 view3 s = [ui]
-    where
-        t = (st s)
-        e1 = F.withFocusRing (_focusRing t) (E.renderEditor (str . unlines)) (_edit1 t)
-        e2 = F.withFocusRing (_focusRing t) (E.renderEditor (str . unlines)) (_edit2 t)
+  where
+    t = (st s)
+    e1 = F.withFocusRing (_focusRing t) (E.renderEditor (str . unlines)) (_edit1 t)
+    e2 = F.withFocusRing (_focusRing t) (E.renderEditor (str . unlines)) (_edit2 t)
 
-        ui = C.center $ 
-            (str "IP: " <+> (hLimit 20 $ vLimit 1 e1)) <=>
-            str " " <=>
-            (str "Port: " <+> (hLimit 5 $ vLimit 1 e2)) <=>
-            str " " <=>
-            str (st2msg s)
+    ui =
+      C.center $
+        (str "IP: " <+> (hLimit 20 $ vLimit 1 e1))
+          <=> str " "
+          <=> (str "Port: " <+> (hLimit 5 $ vLimit 1 e2))
+          <=> str " "
+          <=> str (st2msg s)
 
 view4 s = [C.center $ padRight (Pad 2) (drawStats s)]
 
 -- view1 :: PlayState -> Widget String
--- view1 s = 
+-- view1 s =
 --   withBorderStyle unicode $
 --     borderWithLabel (str (header s)) $
 --       vTile [ mkRow s row | row <- [1..dim] ]
@@ -73,11 +71,12 @@ view1 g@PS {isNetwork = s} = case s of
 
 -- drawStats :: Game -> Widget Name
 drawStats g@PS {gameState = d} = case d of
-  1 -> hLimit 11 $
-        vBox
-          [ drawScore (score g),
-            padTop (Pad 2) $ emptyWidget
-          ]
+  1 ->
+    hLimit 11 $
+      vBox
+        [ drawScore (score g),
+          padTop (Pad 2) $ emptyWidget
+        ]
   4 -> drawGameOver g
 
 drawScore n =
@@ -102,8 +101,8 @@ drawGrid g =
     cellsInRow y = [drawCoord (V2 x y) | x <- [0 .. width -1]]
     drawCoord = drawCell . cellAt
     cellAt c
-      | c `elem` (bird2 g) = Bird
-      | c `elem` (bird1 g) = Bird
+      | c `elem` bird2 g = BirdTwo
+      | c `elem` bird1 g = Bird
       | isPillar g c = Snake
       | otherwise = Empty
 
@@ -116,7 +115,7 @@ drawGridSingle g =
     cellsInRow y = [drawCoord (V2 x y) | x <- [0 .. width -1]]
     drawCoord = drawCell . cellAt
     cellAt c
-      | c `elem` (bird1 g) = Bird
+      | c `elem` bird1 g = Bird
       | isPillar g c = Snake
       | isBonus g c = Bonus
       | otherwise = Empty
@@ -133,6 +132,7 @@ isBonus g@PS {bonus = (V2 xb yb)} (V2 x y) = xb == x && yb == y
 
 drawCell Snake = withAttr snakeAttr cw
 drawCell Bird = withAttr birdAttr cw
+drawCell BirdTwo = withAttr bird2Attr cw
 drawCell Bonus = withAttr bonusAttr cw
 drawCell Empty = withAttr emptyAttr cw
 
