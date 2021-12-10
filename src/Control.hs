6d0e1f16a5864c5141d1291922b4e0f8107cac26
w5cao <w5cao@ucsd.edu>
2021-12-10 13:14:46 -0800

    move test to src like the previous homework

--- src/Control.hs
+++ src/Control.hs
@@ -37,6 +37,7 @@ import Network.Socket.ByteString (recv, sendAll)
 import System.IO
 import System.IO.Unsafe
 import System.Random (Random (..), getStdRandom, newStdGen)
+import Test.QuickCheck
 
 -------------------------------------------------------------------------------
 
@@ -537,3 +538,50 @@ runClient ip port = do
           threadDelay 100000
 
           rrLoop sock
+
+
+
+--------------
+testSortDes :: [Int] -> Bool
+testSortDes xs = reverse (sort xs) == sortDes xs
+  where 
+    types = xs :: [Int]
+
+-- >>> quickCheck testSortDes
+-- +++ OK, passed 100 tests.
+--
+
+
+testNextPZero :: Int -> Int -> Int -> Property
+testNextPZero x rp p = 
+  (x == 0) ==> (nextP x rp p == rp)
+
+testNextPNonZero :: Int -> Int -> Int -> Property
+testNextPNonZero x rp p = 
+  (x /= 0) ==> (nextP x rp p == p)
+
+
+-- >>> quickCheck testNextPNonZero
+-- +++ OK, passed 100 tests; 13 discarded.
+
+-- >>> quickCheck testNextPZero
+-- *** Gave up! Passed only 26 tests; 1000 discarded tests.
+--
+
+
+
+testCollideX :: Int -> Int -> Int -> Int -> Property
+testCollideX bx by px py = 
+  (bx /= px) ==> (collide bx by px py == False)
+
+testCollideY :: Int -> Int -> Int -> Int -> Property
+testCollideY bx by px py = 
+  (by `elem` [py+1 .. py+gapSize-1]) ==> (collide bx by px py == False)
+
+-- >>> quickCheck testCollideX
+-- +++ OK, passed 100 tests; 12 discarded.
+--
+
+-- >>> quickCheck testCollideY
+-- +++ OK, passed 100 tests; 659 discarded.
+--
